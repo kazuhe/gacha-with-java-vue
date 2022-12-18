ec54edaf901cea241968b5a380b930c00d7c9135
kazuhe <omc.ohara@gmail.com>
2022-12-18 16:33:14 +0900

    Spring initializr でプロジェクトを作成

---
+++ src/test/java/com/gacha/gacha/GachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gacha.gacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
